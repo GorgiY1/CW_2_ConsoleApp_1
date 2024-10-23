fd8918fa72c08125a07dc245a82e5478cd390c68
johnny <orgeorgij@gmail.com>
2024-10-23 11:20:37 +0300

    Add project files.

---
+++ ConsoleApp_L02/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ConsoleApp_L02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("");
+
+            //int number = 0;
+
+            //Console.WriteLine("Input number: ");
+
+            ////Console.Read();
+            ////ConsoleKeyInfo consoleKey = Concole.ReadKey();
+
+            ////string str = Console.ReadLine();
+
+            //number = int.Parse(Console.ReadLine());
+            //number = Convert.ToInt32(Console.ReadLine());
+
+            //double d = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine(d);
+
+            //object f = "dasfsd";
+            //Console.WriteLine(f);
+
+            //Console.WriteLine(3 / 8);// 0
+            //Console.WriteLine(3/8.0); // 0.375
+            //Console.WriteLine(24 % 4); // 0
+            //Console.WriteLine(36 % 5); // 1
+            //Console.WriteLine(3 % 5); // 3
+            //// A % B = A - B*(A/B)
+            //Console.WriteLine(33 % 5.2); // 1,8
+            //Console.WriteLine(33 % 5.2);
+            //Console.WriteLine(5.3 / 0);
+            //Console.WriteLine(double.IsInfinity(5.3/0));
+
+            //string str = null;
+
+            //int? number = 24;
+            //number = null;
+
+            //double? d = null;
+            Console.ReadKey();
+        }
+    }
+}
